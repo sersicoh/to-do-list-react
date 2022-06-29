9b3f277ec30da88e123c3d58d1b89c5851c757f0
Wojciech Szymczak <sersicoh@gmail.com>
2022-06-29 18:16:06 +0200

    remove base content

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ import Container from "./Container";
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "przejść na Reacta", done: false },
-    { id: 2, content: "zjeść kolację", done: true },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
