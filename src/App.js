e9a94b16a0f6f6aba16d7f3b24e9710b9b8ac5c6
Wojciech Szymczak <sersicoh@gmail.com>
2022-07-08 15:20:13 +0200

    move functions to prepareTasks.js

--- src/App.js
+++ src/App.js
@@ -5,46 +5,23 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
+import { usePrepareTasks } from "./prepareTasks.js";
 
 function App() {
 
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([]);
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
 
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const togeleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, done: !task.done };
-      }
-      return task;
-    }));
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true,
-    })))
-  };
-
-  const addNewTask = (content) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-        content,
-        done: false,
-      },
-    ]);
-  };
+  const {
+    tasks,
+    togeleTaskDone,
+    removeTask,
+    setAllDone,
+    addNewTask
+  } = usePrepareTasks();
 
   return (
     <Container>
