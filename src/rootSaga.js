96b137c5871bef0b54431a5b216d0bd915d62b64
Wojciech Szymczak <sersicoh@gmail.com>
2022-08-05 16:13:54 +0200

    rename function

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,8 @@
 import {all} from "redux-saga/effects";
-import { watchfetchExampleTasks } from "./features/tasks/tasksSaga";
+import { tasksSaga } from "./features/tasks/tasksSaga";
 
 export default function* rootSaga(){
     yield all([
-        watchfetchExampleTasks(),
+        tasksSaga(),
     ]);
 };
