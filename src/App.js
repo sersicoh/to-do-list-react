72342421fd6c109cb3a6ddce7f94c9f81f110413
Wojciech Szymczak <sersicoh@gmail.com>
2022-08-06 17:43:42 +0200

    add content as React Router in App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,35 @@
-import Tasks from "./features/tasks/Tasks";
+import { Switch, Route, HashRouter, NavLink, Redirect } from "react-router-dom";
+import AuthorPage from "./features/author/AuthorPage";
+import TaskPage from "./features/tasks/TaskPage";
+import TasksPage from "./features/tasks/TasksPage";
+import { StyledNavLink } from "./styled";
 
-export default () => <Tasks />
+export default () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/autor">O autorze</StyledNavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania/:id">
+          <TaskPage />
+        </Route>
+        <Route path="/zadania">
+          <TasksPage />
+        </Route>
+        <Route path="/autor">
+          <AuthorPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/zadania">
+          </Redirect>
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
