fcbc3c243643cdd4ea099930b25d5321340dba47
Wojciech Szymczak <sersicoh@gmail.com>
2022-08-08 17:57:26 +0200

    refactor to Buttons as component features

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -1,34 +1,62 @@
 import Form from "./Form";
 import List from "./List";
-import Buttons from "./Buttons";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
 import Container from "../../../common/Container";
 import Search from "./Search";
+import Buttons from "../../Buttons";
+import { ActionButtons } from "../../Buttons/styled";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchExampleTasks, selectAreTaskEmpty, selectHideDone, selectIsEveryTaskDone, setAllDone, toggleHideDone } from "../tasksSlice";
+
 
 function TasksPage() {
 
-  return (
-    <Container>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form />}
-      />
-      <Section
-        title="Wyszukaj zadanie"
-        body={<Search />}
-      />
-      <Section
-        title="Lista zadań"
-        body={
-          <List />}
-        extraHeaderContent={
-          <Buttons />
-        }
-      />
-    </Container>
-  );
+   const areTasksEmpty = useSelector(selectAreTaskEmpty);
+   const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+   const hideDone = useSelector(selectHideDone);
+
+   const dispatch = useDispatch();
+
+   return (
+      <Container>
+         <Header title="Lista zadań" />
+         <Section
+            title="Dodaj nowe zadanie"
+            body={<Form />}
+            extraHeaderContent={
+               <Buttons
+                  query={"Pobierz przykładowe zadania"}
+                  onClick={() => dispatch(fetchExampleTasks())}
+               />
+            }
+         />
+         <Section
+            title="Wyszukaj zadanie"
+            body={<Search />}
+         />
+         <Section
+            title="Lista zadań"
+            body={
+               <List />}
+            extraHeaderContent={
+               !areTasksEmpty && (
+                  <ActionButtons>
+                     <Buttons
+                        query={`${hideDone ? "Pokaż" : "Ukryj"} ukończone`}
+                        onClick={() => dispatch(toggleHideDone())}
+                     />
+                     <Buttons
+                        query={"Ukończ wszystkie"}
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={isEveryTaskDone}
+                     />
+                  </ActionButtons>
+               )
+            }
+         />
+      </Container>
+   );
 }
 
 export default TasksPage;
